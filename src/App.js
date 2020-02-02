a484fd1dbdcfd7e6ce467a420cf8b5efb8b7b4cb
andreLagacione <lagacioneandre@gmail.com>
2020-02-02 18:30:32 -0300

    add new features

--- src/App.js
+++ src/App.js
@@ -1,94 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './global.css';
 import './App.css';
 import './sidebar.css';
 import './main.css';
 
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
+
 function App() {
+  
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data);
+    setDevs([...devs, response.data]);
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
-
+        <DevForm onSubmit={handleAddDev} />
       </aside>
 
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/10717151?s=460&v=4" alt="André Ricardo Lagacione" />
-              <div className="user-info">
-                <strong>André Ricardo Lagacione</strong>
-                <span>Angular, Flutter, Java, Spring boot</span>
-              </div>
-            </header>
-
-            <p>Full stack developer!</p>
-            <a href="https://github.com/andreLagacione">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/10717151?s=460&v=4" alt="André Ricardo Lagacione" />
-              <div className="user-info">
-                <strong>André Ricardo Lagacione</strong>
-                <span>Angular, Flutter, Java, Spring boot</span>
-              </div>
-            </header>
-
-            <p>Full stack developer!</p>
-            <a href="https://github.com/andreLagacione">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/10717151?s=460&v=4" alt="André Ricardo Lagacione" />
-              <div className="user-info">
-                <strong>André Ricardo Lagacione</strong>
-                <span>Angular, Flutter, Java, Spring boot</span>
-              </div>
-            </header>
-
-            <p>Full stack developer!</p>
-            <a href="https://github.com/andreLagacione">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/10717151?s=460&v=4" alt="André Ricardo Lagacione" />
-              <div className="user-info">
-                <strong>André Ricardo Lagacione</strong>
-                <span>Angular, Flutter, Java, Spring boot</span>
-              </div>
-            </header>
-
-            <p>Full stack developer!</p>
-            <a href="https://github.com/andreLagacione">Acessar perfil no Github</a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
       </main>
     </div>
